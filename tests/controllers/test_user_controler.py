05f689fc2d4e3dcac3d1f5327de28e61cf6cf4b9
Kauan Gross <kauangross@gmail.com>
2025-06-04 21:16:18 -0300

    teste

--- tests/controllers/test_user_controler.py
+++ tests/controllers/test_user_controler.py
@@ -4,61 +4,19 @@ import pytest
     
 client = TestClient(app)
 
-
-def test_read_item():
-    response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
-    assert response.status_code == 200
-    assert response.json() == {
-        "id": "foo",
-        "title": "Foo",
-        "description": "There goes my hero",
-    }
-
-def test_read_item_bad_token():
-    response = client.get("/items/foo", headers={"X-Token": "hailhydra"})
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Invalid X-Token header"}
-
-
-def test_read_nonexistent_item():
-    response = client.get("/items/baz", headers={"X-Token": "coneofsilence"})
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Item not found"}
-
-
-def test_create_item():
+@pytest.mark.parametrize("username,email,password", [
+    ("kauan", "kauangross@gmail.com", "123567")
+])
+def test_create_user_deve_criar(username,email,password):
     response = client.post(
-        "/items/",
-        headers={"X-Token": "coneofsilence"},
-        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
-    )
-    assert response.status_code == 200
-    assert response.json() == {
-        "id": "foobar",
-        "title": "Foo Bar",
-        "description": "The Foo Barters",
-    }
+        "/api/v1/users",
+        headers={"accept" : "application/json",
+                 "Content-Type" : "application/json"},
 
-
-def test_create_item_bad_token():
-    response = client.post(
-        "/items/",
-        headers={"X-Token": "hailhydra"},
-        json={"id": "bazz", "title": "Bazz", "description": "Drop the bazz"},
-    )
-    assert response.status_code == 400
-    assert response.json() == {"detail": "Invalid X-Token header"}
-
-
-def test_create_existing_item():
-    response = client.post(
-        "/items/",
-        headers={"X-Token": "coneofsilence"},
         json={
-            "id": "foo",
-            "title": "The Foo ID Stealers",
-            "description": "There goes my stealer",
+            "username": username,
+            "email": email,
+            "password": password
         },
     )
-    assert response.status_code == 409
-    assert response.json() == {"detail": "Item already exists"}
+    assert response.status_code == 200
