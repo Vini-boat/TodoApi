4748e18dc70d903aa6463388dbb4d4a205faaf2b
Kauan Gross <kauangross@gmail.com>
2025-06-11 21:12:38 -0300

    .

--- tests/controllers/test_user_controler.py
+++ tests/controllers/test_user_controler.py
@@ -7,21 +7,23 @@ from app.infraestructure.sqlite import get_db_session
 
 app.dependency_overrides[get_db_session] = mock_get_db_session
 
-client = TestClient(app)
+
 
 @pytest.mark.parametrize("username,email,password", [
     ("kauan", "kauangross@gmail.com", "123567")
 ])
 def test_create_user_deve_criar(username,email,password):
-    response = client.post(
-        "/api/v1/users",
-        headers={"accept" : "application/json",
-                 "Content-Type" : "application/json"},
+    with TestClient(app) as client:
+        
+        response = client.post(
+            "/api/v1/users",
+            headers={"accept" : "application/json",
+                    "Content-Type" : "application/json"},
 
-        json={
-            "username": username,
-            "email": email,
-            "password": password
-        },
-    )
-    assert response.status_code == 200
+            json={
+                "username": username,
+                "email": email,
+                "password": password
+            },
+        )
+        assert response.status_code == 200
